64d43ad763b90663d70481aa42f62176b21c613c
Carjul <emugameplay.tv@gmail.com>
2025-08-18 22:24:19 -0500

    fghjk

---
+++ api/public/calcular_pos/assets/demo.js
@@ -0,0 +1,26 @@
+
+
+
+localStorage.setItem("result", 0);
+
+var posicion = 0;  
+
+function guardar() {
+    var consecutivo = document.getElementById("numero").value;
+    var pos =document.getElementById("posicion").value;
+    
+    localStorage.setItem("result",  (parseInt(consecutivo) - parseInt(pos))  );
+    document.getElementById("numero").value = "";
+    document.getElementById("posicion").value = "";
+    document.getElementById("numero").focus();
+}
+
+function buscar() {
+    var clave = document.getElementById("buscar_posicion").value;
+     document.getElementById("resultado").innerHTML = parseInt(localStorage.getItem("result")) + parseInt(clave);
+}
+
+function reinicio() {
+localStorage.setItem("result", 0);
+document.getElementById("resultado").innerHTML = "0";
+}
